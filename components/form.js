a8c19f7b72cac9275624155519692a75c57b3283
Daniel Martinsen <daniel.martinsen@online.no>
2020-05-02 18:45:54 +0200

    Updating registration form

--- components/form.js
+++ components/form.js
@@ -1,26 +1,6 @@
 import styles from '../styles/form.module.scss'
-import React from 'react'
-import Radio from '@material-ui/core/Radio'
-import RadioGroup from '@material-ui/core/RadioGroup'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
-import { withStyles } from '@material-ui/core'
-
-const StyledRadio = withStyles({
-  root: {
-    color: '#0f152d',
-    '&$checked': {
-      color: '#0f152d',
-    },
-  },
-})((props) => <Radio color='default' {...props} />)
 
 export default function form() {
-  const [value, setValue] = React.useState('')
-
-  const handleChange = (event) => {
-    setValue(event.target.value)
-  }
-
   return (
     <form className={styles.registrerForm}>
       <input type='text' placeholder='Fornavn' name='Fornavn' className={styles.inputField} required />
@@ -36,16 +16,18 @@ export default function form() {
         required
       />
 
-      <RadioGroup
-        aria-label='kjonn'
-        name='kjonn'
-        value={value}
-        onChange={handleChange}
-        className={styles.registrerKjonn}>
-        <FormControlLabel value='Gutt' control={<StyledRadio />} label='Gutt' fontSize='25px' />
-        <FormControlLabel value='Jente' control={<StyledRadio />} label='Jente' className={styles.velgKjonn} />
-        <FormControlLabel value='Annet' control={<StyledRadio />} label='Annet' className={styles.velgKjonn} />
-      </RadioGroup>
+      <div className={styles.registrerKjonn}>
+        <p className={styles.forklaring}>Velg kjønn: </p>
+
+        <input type='radio' name='kjønn' value='Gutt' id='gutt' className={styles.velgKjonn} required />
+        <label for='gutt'>Gutt</label>
+
+        <input type='radio' name='kjønn' value='Jente' id='jente' className={styles.velgKjonn} required />
+        <label for='jente'>Jente</label>
+
+        <input type='radio' name='kjønn' value='Annet' id='annet' className={styles.velgKjonn} required />
+        <label for='annet'>Annet</label>
+      </div>
 
       <button type='submit' className={styles.button}>
         Registrer meg!
