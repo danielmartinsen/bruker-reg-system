c0796c4e292bf6df691de9b75ef29fcd0c334132
Daniel Martinsen <daniel.martinsen@online.no>
2020-05-03 18:31:25 +0200

    Updating registration form

--- components/form.js
+++ components/form.js
@@ -3,28 +3,33 @@ import styles from '../styles/form.module.scss'
 export default function form() {
   return (
     <form className={styles.registrerForm}>
-      <input type='text' placeholder='Fornavn' name='Fornavn' className={styles.inputField} required />
-      <input type='text' placeholder='Etternavn' name='Etternavn' className={styles.inputField} required />
-      <input type='number' placeholder='Telefon' name='Telefon' className={styles.inputField} required />
-      <input
-        type='text'
-        placeholder='Bursdag'
-        name='Bursdag'
-        onFocus={(e) => (e.target.type = 'date')}
-        onBlur={(e) => (e.target.type = 'text')}
-        className={styles.inputField}
-        required
-      />
+      <div className={styles.container}>
+        <input type='text' placeholder='Fornavn' name='Fornavn' className={styles.inputField} required />
+        <input type='text' placeholder='Etternavn' name='Etternavn' className={styles.inputField} required />
+      </div>
 
-      <div className={styles.registrerKjonn}>
+      <div className={styles.container}>
+        <input type='number' placeholder='Telefon' name='Telefon' className={styles.inputField} required />
+        <input
+          type='text'
+          placeholder='Bursdag'
+          name='Bursdag'
+          onFocus={(e) => (e.target.type = 'date')}
+          onBlur={(e) => (e.target.type = 'text')}
+          className={styles.inputField}
+          required
+        />
+      </div>
+
+      <div className={styles.container}>
         <input type='radio' name='kjønn' value='Gutt' id='gutt' className={styles.velgKjonn} required />
-        <label for='gutt'>Gutt</label>
+        <label htmlFor='gutt'>Gutt</label>
 
         <input type='radio' name='kjønn' value='Jente' id='jente' className={styles.velgKjonn} required />
-        <label for='jente'>Jente</label>
+        <label htmlFor='jente'>Jente</label>
 
         <input type='radio' name='kjønn' value='Annet' id='annet' className={styles.velgKjonn} required />
-        <label for='annet'>Annet</label>
+        <label htmlFor='annet'>Annet</label>
       </div>
 
       <button type='submit' className={styles.button}>
