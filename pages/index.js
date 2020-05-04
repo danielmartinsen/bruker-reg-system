70ffcd43f6ef1c24988d158c4feb918463c32581
Daniel Martinsen <daniel.martinsen@online.no>
2020-05-04 09:55:01 +0200

    Testing environment variable

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from '../components/layout'
 export default function Home() {
   return (
     <Layout title='Tast brukernummeret ditt'>
-      <div></div>
+      <div>{process.env.LICENSE_KEY}</div>
     </Layout>
   )
 }
