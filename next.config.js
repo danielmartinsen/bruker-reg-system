05b506dd922f204acc25a8bf712ed6680bfa037f
Daniel Martinsen <daniel.martinsen@online.no>
2020-05-06 10:24:17 +0200

    Testing env variabøes

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  env: {
+    LICENSE_KEY: process.env.LICENSE_KEY,
+  },
+}
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,10 @@ import Layout from '../components/layout'
 import Numpad from '../components/numpad'
 
 export default function Home() {
-  const License = process.env.REACT_APP_LICENSE
-
   return (
     <Layout title=''>
       <Numpad />
-      <p>{'https://api.danielmartinsen.now.sh/' + License + '/brukere/get/1'}</p>
+      <p>{process.env.LICENSE_KEY}</p>
     </Layout>
   )
 }
