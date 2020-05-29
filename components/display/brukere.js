4175e5103a0ecae4062283f24fd15d70c152e66d
Daniel Martinsen <daniel.martinsen@online.no>
2020-05-29 18:53:18 +0200

    Update display screen

--- components/display/brukere.js
+++ components/display/brukere.js
@@ -1,6 +1,6 @@
 import styles from '../../styles/display/infobox.module.scss'
 import { loadFirebase } from '../../lib/firebase'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
 export default function AntallBrukere() {
   const firebase = loadFirebase()
@@ -13,25 +13,42 @@ export default function AntallBrukere() {
   var license = ''
   useEffect(() => {
     license = localStorage.getItem('LicenseKey')
+  })
 
-    const update = setInterval(() => {
-      db.collection('Kunder')
-        .doc(license)
-        .collection('Logg')
-        .doc(idag)
-        .get()
-        .then((doc) => {
-          var antallBrukere = 0
-          for (var bruker in doc.data()) {
-            if (doc.data()[bruker].dato == idag) {
-              antallBrukere++
-              setAntall(antallBrukere)
-            }
+  function useInterval(callback, delay) {
+    const savedCallback = useRef()
+
+    useEffect(() => {
+      savedCallback.current = callback
+    }, [callback])
+
+    useEffect(() => {
+      function tick() {
+        savedCallback.current()
+      }
+      if (delay !== null) {
+        let id = setInterval(tick, delay)
+        return () => clearInterval(id)
+      }
+    }, [delay])
+  }
+
+  useInterval(() => {
+    db.collection('Kunder')
+      .doc(license)
+      .collection('Logg')
+      .doc(idag)
+      .get()
+      .then((doc) => {
+        var antallBrukere = 0
+        for (var bruker in doc.data()) {
+          if (doc.data()[bruker].dato == idag) {
+            antallBrukere++
+            setAntall(antallBrukere)
           }
-        })
-    }, 1000)
-    return () => clearInterval(update)
-  }, [])
+        }
+      })
+  }, 1000)
 
   function update() {
     console.log('Kjører nå')
