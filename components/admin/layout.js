ee39e848760dcdd531cf99c6d11cc5bccbfd53d8
Daniel Martinsen <daniel.martinsen@online.no>
2020-06-07 14:57:35 +0200

    Clean-up in code

--- components/admin/layout.js
+++ components/admin/layout.js
@@ -1,10 +1,9 @@
+import { useState, useEffect } from 'react'
+import Router from 'next/router'
 import Head from 'next/head'
 import Nav from './nav'
 
-import { useState, useEffect } from 'react'
-import Router from 'next/router'
-
-const siteTitle = 'Administrasjonspanel'
+const siteTitle = 'U-reg Admin'
 
 export default function Layout({ children }) {
   const [login, setLogin] = useState()
--- components/admin/nav.js
+++ components/admin/nav.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
-import styles from '../../styles/admin/nav.module.scss'
 import Router from 'next/router'
+import styles from '../../styles/admin/nav.module.scss'
 
 export default function Nav() {
   function logout() {
